0c659386148794bed2fe03863b014fb86562abd9
Justin Yoon <justinyoon@Justin-Air.local>
2023-04-06 15:34:37 -0700

    initial files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import { useState } from 'react';
 import './App.css';
+import { AddTransaction } from './components/AddTransaction';
+import { Balance } from './components/Balance';
+import { Header } from './components/Header';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+
+import { GlobalProvider } from './context/GlobalState';
 
 function App() {
   return (
-    <div className="App">
-      <p> Hello World</p>
-    </div>
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
   );
 }
 
